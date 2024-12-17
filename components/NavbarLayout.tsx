87d57becc4d6a9338f9064a20eee8afba22853a4
Alexandre Malet <alexandre@gowod.app>
2024-12-17 12:37:27 +0100

    analytics

--- components/NavbarLayout.tsx
+++ components/NavbarLayout.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import Navbar from "./Navbar";
+import React from 'react';
+import { Analytics } from '@vercel/analytics/next';
+
+import Navbar from './Navbar';
 
 type NavbarLayoutType = {
   children: React.ReactNode;
@@ -8,6 +10,7 @@ export default function NavbarLayout({ children }: NavbarLayoutType) {
   return (
     <>
       <Navbar />
+      <Analytics />
       {children}
     </>
   );
